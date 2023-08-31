1a3cd4eb90c7eee7ddd1ffce2b5d7c2d6cbbabee
yaredghebre <yared2391@gmail.com>
2023-08-31 13:05:10 -0300

    Add: Home almost complete

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,34 @@ import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
 
 const Home = () => {
-  return <div className="w-full h-screen bg-[#0a192f]"></div>;
+  return (
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
+      {/* Container */}
+      <div className="max-w-[1080px] mx-auto px-8 flex flex-col justify-center h-full">
+        <h3 className="text-[#29A38E] text-xl sm:text-2xl">Hi! My name is </h3>
+        <h1 className="text-4xl sm:text-7xl font-bold text-white py-4">
+          Yared Ghebreghergis
+        </h1>
+        <h2 className="text-2xl sm:text-5xl font-bold text-[#8892b0]">
+          I'm a Jr. Full Stack Web Developer,
+        </h2>
+        <p className="text-[#8892b0] text-xl sm:text-2xl py-4">
+          who enjoys building digital experiences. Currently, I'm focused on
+          recreating responsive web applications, mostly on the front-end side.{" "}
+          <br />I always love learning to use new updated and innovative tools!
+          Here are some of my recent projects:
+        </p>
+        <div>
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#29A38E] hover:border-[#29A38E]">
+            Projects
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-2" />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
