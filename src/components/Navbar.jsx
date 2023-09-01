56a3eab9f956925ea08ae1ea1a0ae6768f1dfd66
yaredghebre <yared2391@gmail.com>
2023-09-01 12:28:56 -0300

    Add: links to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,19 @@ import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import Logo from "../assets/profile-pic.png";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
   const [navOpen, setNavOpen] = useState(false);
+  const [activeLink, setActiveLink] = useState("");
   const toggleNav = () => setNavOpen(!navOpen);
 
+  const handleLinkClick = (linkName) => {
+    setActiveLink(linkName);
+  };
+
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-[20px] bg-[#0a192f21] text-gray-300">
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-[20px] bg-gray-700 text-gray-300">
       <div className="h-[50px] w-[50px] border rounded-full overflow-hidden">
         <img
           src={Logo}
@@ -18,38 +24,158 @@ const Navbar = () => {
         />
       </div>
 
-      {/* MENU */}
+      {/* ----------- NAVBAR MENU --------- */}
 
-      <ul className="hidden md:flex gap-3 cursor-pointer">
-        <li>Home</li>
-        <li>About</li>
-        <li>Skills</li>
-        <li>Projects</li>
-        <li>Contact</li>
+      <ul className="hidden md:flex gap-3 cursor-pointer text-xl">
+        <li>
+          <Link
+            to="home"
+            smooth={true}
+            offset={50}
+            duration={500}
+            className={activeLink === "Home" ? "active-link" : ""}
+            onClick={() => handleLinkClick("Home")}
+          >
+            Home
+          </Link>
+        </li>
+
+        <li>
+          {" "}
+          <Link
+            to="about"
+            smooth={true}
+            offset={50}
+            duration={500}
+            className={activeLink === "About" ? "active-link" : ""}
+            onClick={() => handleLinkClick("About")}
+          >
+            About
+          </Link>
+        </li>
+
+        <li>
+          {" "}
+          <Link
+            to="skills"
+            smooth={true}
+            offset={50}
+            duration={500}
+            className={activeLink === "Skills" ? "active-link" : ""}
+            onClick={() => handleLinkClick("Skills")}
+          >
+            Skills
+          </Link>
+        </li>
+
+        <li>
+          {" "}
+          <Link
+            to="projects"
+            smooth={true}
+            offset={50}
+            duration={500}
+            className={activeLink === "Projects" ? "active-link" : ""}
+            onClick={() => handleLinkClick("Projects")}
+          >
+            Projects
+          </Link>
+        </li>
+
+        <li>
+          {" "}
+          <Link
+            to="contact"
+            smooth={true}
+            offset={50}
+            duration={500}
+            className={activeLink === "Contact" ? "active-link" : ""}
+            onClick={() => handleLinkClick("Contact")}
+          >
+            Contact
+          </Link>
+        </li>
       </ul>
+      {/* --------------------------------- */}
 
       {/* Hamburger */}
       <div onClick={toggleNav} className="md:hidden z-10">
         {!navOpen ? <FaBars /> : <FaTimes />}
       </div>
+      {/* --------------------------------- */}
 
-      {/* Mobile */}
-
+      {/* ----------- MOBILE ---------------*/}
       <div
-        className={`fixed top-0 right-0 w-3/4 h-full bg-[#29A38E] text-gray-300 transform ${
+        className={`fixed top-0 right-0 w-3/4 h-full bg-[#29A38E] text-gray-300 transform z-50 ${
           navOpen ? "translate-x-0" : "translate-x-full"
         } transition-transform ease-in-out duration-300 md:hidden`}
       >
         <ul className="flex flex-col justify-center items-center h-full">
-          <li className="py-6 text-xl text-black hover:text-white">Home</li>
-          <li className="py-6 text-xl text-black hover:text-white">About</li>
-          <li className="py-6 text-xl text-black hover:text-white">Skills</li>
-          <li className="py-6 text-xl text-black hover:text-white">Projects</li>
-          <li className="py-6 text-xl text-black hover:text-white">Contact</li>
+          <li className="py-6 text-xl text-black hover:text-white">
+            <Link
+              to="home"
+              smooth={true}
+              offset={50}
+              duration={500}
+              className={activeLink === "Home" ? "active-link" : ""}
+              onClick={() => handleLinkClick("Home")}
+            >
+              Home
+            </Link>
+          </li>
+          <li className="py-6 text-xl text-black hover:text-white">
+            <Link
+              to="about"
+              smooth={true}
+              offset={50}
+              duration={500}
+              className={activeLink === "About" ? "active-link" : ""}
+              onClick={() => handleLinkClick("About")}
+            >
+              About
+            </Link>
+          </li>
+          <li className="py-6 text-xl text-black hover:text-white">
+            <Link
+              to="skills"
+              smooth={true}
+              offset={50}
+              duration={500}
+              className={activeLink === "Skills" ? "active-link" : ""}
+              onClick={() => handleLinkClick("Skills")}
+            >
+              Skills
+            </Link>
+          </li>
+          <li className="py-6 text-xl text-black hover:text-white">
+            <Link
+              to="projects"
+              smooth={true}
+              offset={50}
+              duration={500}
+              className={activeLink === "Projects" ? "active-link" : ""}
+              onClick={() => handleLinkClick("Projects")}
+            >
+              Projects
+            </Link>
+          </li>
+          <li className="py-6 text-xl text-black hover:text-white">
+            <Link
+              to="contact"
+              smooth={true}
+              offset={50}
+              duration={500}
+              className={activeLink === "Contact" ? "active-link" : ""}
+              onClick={() => handleLinkClick("Contact")}
+            >
+              Contact
+            </Link>
+          </li>
         </ul>
       </div>
+      {/* --------------------------------- */}
 
-      {/* Social Icons */}
+      {/* ---------- LEFT SIDEBAR ---------- */}
       <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
         <ul>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600 px-3">
@@ -93,6 +219,7 @@ const Navbar = () => {
           </li>
         </ul>
       </div>
+      {/* --------------------------------- */}
     </div>
   );
 };
