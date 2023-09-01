2b63e0d9315746afe9f81c933411b2199c77d90c
yaredghebre <yared2391@gmail.com>
2023-09-01 11:53:44 -0300

    Add: Contact from (need getform.io implementation)

---
+++ src/components/Contact.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div
+      name="contact"
+      className="w-full h-screen bg-[#0a192f]  flex justify-center items-center p-4"
+    >
+      <form action="" className="flex flex-col max-w-[600px] w-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-[#29A38E] sm:text-center text-gray-300">
+            Contact
+          </p>
+          <p className="text-gray-300 py-4">// Submit the form below</p>
+        </div>
+        <input className="p-2" type="text" placeholder="Name" name="name" />
+        <input
+          className="my-4 p-2"
+          type="email"
+          placeholder="Email"
+          name="email"
+        />
+        <textarea
+          className="p-2"
+          name="message"
+          rows="10"
+          placeholder="Message"
+        ></textarea>
+        <button className="text-white border-2 hover:bg-[#29A38E] hover:border-[#29A38E] duration-300 rounded px-10 py-3 my-8 mx-auto flex items-center">
+          Send
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
