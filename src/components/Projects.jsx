a23c108a8dfc213880ff9c89a15ca1ca9323aa43
yaredghebre <yared2391@gmail.com>
2023-09-26 09:02:46 +0200

    Projects updated (needs demo)

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,6 +5,9 @@ import BoolFlix from "../assets/projects/BoolFlix.png";
 import BoolzApp from "../assets/projects/BoolzApp.png";
 import MineSweeper from "../assets/projects/MineSweeper.png";
 import Spotify from "../assets/projects/Spotify.png";
+import QuizApp from "../assets/projects/QuizApp.png";
+import PassGen from "../assets/projects/PasswordGen.png";
+import DeliveBoo from "../assets/projects/DeliveBoo-customerSide.png";
 
 const Projects = () => {
   return (
@@ -31,8 +34,8 @@ const Projects = () => {
           >
             {/* Hover Effect */}
             <div className="opacity-0 group-hover:opacity-100 duration-300 overlay-div flex flex-col">
-              <span className="text-4xl mb-2 font-bold text-black">
-                My Project
+              <span className="text-3xl mb-2 font-bold text-black">
+                Calculator App
               </span>
               <div className="flex gap-2">
                 <a href="">
@@ -40,7 +43,7 @@ const Projects = () => {
                     Demo
                   </button>
                 </a>
-                <a href="">
+                <a href="https://github.com/yaredghebre/app-calculator">
                   <button className="text-center rounded-lg p-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
@@ -57,8 +60,8 @@ const Projects = () => {
           >
             {/* Hover Effect */}
             <div className="opacity-0 group-hover:opacity-100 duration-300 overlay-div flex flex-col">
-              <span className="text-4xl mb-2 font-bold text-black">
-                My Project
+              <span className="text-3xl mb-2 font-bold text-black">
+                Avada Gym (Clone)
               </span>
               <div className="flex gap-2">
                 <a href="">
@@ -66,7 +69,7 @@ const Projects = () => {
                     Demo
                   </button>
                 </a>
-                <a href="">
+                <a href="https://github.com/yaredghebre/proj-vuejs-ava-clone">
                   <button className="text-center rounded-lg p-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
@@ -84,7 +87,7 @@ const Projects = () => {
             {/* Hover Effect */}
             <div className="opacity-0 group-hover:opacity-100 duration-300 overlay-div flex flex-col">
               <span className="text-4xl mb-2 font-bold text-black">
-                My Project
+                BoolFlix
               </span>
               <div className="flex gap-2">
                 <a href="">
@@ -92,7 +95,7 @@ const Projects = () => {
                     Demo
                   </button>
                 </a>
-                <a href="">
+                <a href="https://github.com/yaredghebre/vite-boolflix">
                   <button className="text-center rounded-lg p-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
@@ -109,8 +112,8 @@ const Projects = () => {
           >
             {/* Hover Effect */}
             <div className="opacity-0 group-hover:opacity-100 duration-300 overlay-div flex flex-col">
-              <span className="text-4xl mb-2 font-bold text-black">
-                My Project
+              <span className="text-2xl mb-2 font-bold text-black">
+                WhatsApp Web (Clone)
               </span>
               <div className="flex gap-2">
                 <a href="">
@@ -118,7 +121,7 @@ const Projects = () => {
                     Demo
                   </button>
                 </a>
-                <a href="">
+                <a href="https://github.com/yaredghebre/vue-boolzapp">
                   <button className="text-center rounded-lg p-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
@@ -136,7 +139,7 @@ const Projects = () => {
             {/* Hover Effect */}
             <div className="opacity-0 group-hover:opacity-100 duration-300 overlay-div flex flex-col">
               <span className="text-4xl mb-2 font-bold text-black">
-                My Project
+                Mine Sweeper
               </span>
               <div className="flex gap-2">
                 <a href="">
@@ -144,7 +147,7 @@ const Projects = () => {
                     Demo
                   </button>
                 </a>
-                <a href="">
+                <a href="https://github.com/yaredghebre/js-campominato-dom">
                   <button className="text-center rounded-lg p-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
@@ -162,8 +165,8 @@ const Projects = () => {
           >
             {/* Hover Effect */}
             <div className="opacity-0 group-hover:opacity-100 duration-300 overlay-div flex flex-col">
-              <span className="text-4xl mb-2 font-bold text-black">
-                My Project
+              <span className="text-2xl mb-2 font-bold text-black">
+                Spotify Web (Clone)
               </span>
               <div className="flex gap-2">
                 <a href="">
@@ -171,7 +174,7 @@ const Projects = () => {
                     Demo
                   </button>
                 </a>
-                <a href="">
+                <a href="https://github.com/yaredghebre/html-css-spotifyweb">
                   <button className="text-center rounded-lg p-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
@@ -179,6 +182,89 @@ const Projects = () => {
               </div>
             </div>
           </div>
+
+          {/* ---------- */}
+
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${QuizApp})` }}
+            className="group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effect */}
+            <div className="opacity-0 group-hover:opacity-100 duration-300 overlay-div flex flex-col">
+              <span className="text-4xl mb-2 font-bold text-black">
+                QuizApp
+              </span>
+              <div className="flex gap-2">
+                <a href="">
+                  <button className="text-center rounded-lg p-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/yaredghebre/quiz-app-react">
+                  <button className="text-center rounded-lg p-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* ---------- */}
+
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${PassGen})` }}
+            className="group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effect */}
+            <div className="opacity-0 group-hover:opacity-100 duration-300 overlay-div flex flex-col">
+              <span className="text-2xl mb-2 font-bold text-black">
+                Password Generator (PHP)
+              </span>
+              <div className="flex gap-2">
+                <a href="">
+                  <button className="text-center rounded-lg p-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/yaredghebre/php-strong-password-generator">
+                  <button className="text-center rounded-lg p-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* ---------- */}
+
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${DeliveBoo})` }}
+            className="group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effect */}
+            <div className="opacity-0 group-hover:opacity-100 duration-300 overlay-div flex flex-col">
+              <span className="text-4xl mb-2 font-bold text-black">
+                DeliveBoo
+              </span>
+              <div className="flex gap-2">
+                <a href="https://www.linkedin.com/feed/update/urn:li:activity:7089611484103622656/">
+                  <button className="text-center rounded-lg p-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/yaredghebre/DeliveBoo-frontend">
+                  <button className="text-center rounded-lg p-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* ---------- */}
         </div>
       </div>
     </div>
